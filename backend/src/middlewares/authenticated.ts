e81b8b28c55ea9833ba9f400285405fa350d1971
Allan Scala <allanscala@gmail.com>
2022-02-28 12:10:13 -0300

    fix: remove comment codes in authenticate

--- backend/src/middlewares/authenticated.ts
+++ backend/src/middlewares/authenticated.ts
@@ -21,23 +21,3 @@ app.use(function(req: Request, res: Response, next: NextFunction) {
 })
 
 export default app
-
-/* export class Authenticated {
-
-    async authenticate(req: Request, res: Response, next: NextFunction) {
-        const authorization = req.headers.authorization
-
-        if(!authorization) {
-            return res.send({message: "Usuário não autorizado."})
-        }
-
-        const token = authorization.split(" ")
-
-        try {
-            verify(token[1], `${process.env.SECRET_TOKEN}`)
-            return next()
-        } catch (error) {
-            return res.send({message: "Sua sessão expirou, faça o login novamente."})
-        }
-    }
-} */
