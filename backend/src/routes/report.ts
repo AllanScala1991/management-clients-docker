9d77ae33d66082f4f2d89e6896352de57a1dd910
Allan Scala <allanscala@gmail.com>
2022-03-04 14:14:50 -0300

    fix: include auth in report route

--- backend/src/routes/report.ts
+++ backend/src/routes/report.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from "express";
 import { ReportController } from "controllers/report";
+import auth from "middlewares/authenticated"
 
 const app = Router()
 
 
-app.get("/report/customer/:userId", async (req: Request, res: Response) => {
+app.get("/report/customer/:userId", auth, async (req: Request, res: Response) => {
     const userId = req.params.userId
 
     const mountCustomerReport = await new ReportController().mountCustomersReport(userId)
