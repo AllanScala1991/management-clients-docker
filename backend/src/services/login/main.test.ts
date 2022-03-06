dee8dd8db6ef367aa4fdc0b4bc13e3b623348640
Allan Scala <allanscala@gmail.com>
2022-03-06 16:51:33 -0300

    feat: create login test

--- backend/src/services/login/main.test.ts
+++ backend/src/services/login/main.test.ts
@@ -12,4 +12,26 @@ describe("Login Service", () => {
         expect(login.status).toBeTruthy
         expect(login.token.length).toBeGreaterThan(0)
     })
+
+    it("Should validate login with invalid username", async () => {
+        const login = await new LoginService().login(
+            "invalid username",
+            `${process.env.PASSWORD}`
+        )
+
+        expect(login.status).toBeFalsy
+        expect(login).not.toHaveProperty
+    })
+
+    it("Should validate login with invalid password", async() => {
+        const login = await new LoginService().login(
+            `${process.env.USERNAME}`,
+            "invalid_password"
+        )
+
+        expect(login.status).toBeFalsy
+        expect(login).not.toHaveProperty
+    })
+
+    
 })
