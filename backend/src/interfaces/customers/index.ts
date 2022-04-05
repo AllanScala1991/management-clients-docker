3c5aad08e48afadfa37fa642cfc4b8809e1a48b6
Allan Scala <allanscala@gmail.com>
2022-04-04 18:26:07 -0700

    fix: adjust git ignore

---
+++ backend/src/interfaces/customers/index.ts
@@ -0,0 +1,13 @@
+export interface ICustomer {
+    name: string,
+    birthDate: string,
+    zipCode: string,
+    city: string,
+    district: string,
+    address: string,
+    addressNumber: number,
+    state: string,
+    phone: string,
+    email: string,
+    userId: string
+}
